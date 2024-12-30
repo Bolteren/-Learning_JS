039da5cf967efc692172fce0ca0cc5e561e92ec7
Bolter <BolterHell@gmail.com>
2024-12-30 20:35:32 +0500

    add index and js  file

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const x = 45;
+const y = 123.897;
+const z = -0.123;
+
+const num1 = 0b1011;
+
+const num2 = 0o11;
+const num3 = 0xff;
+const num4 = 123_4567;
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);

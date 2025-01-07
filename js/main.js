e2ae820a31d90c25bad8b80655c36c0f4e30124e
Bolter <BolterHell@gmail.com>
2025-01-07 20:41:45 +0500

    Lesson metod

--- js/main.js
+++ js/main.js
@@ -1,29 +1,9 @@
-//Объекты
-
+//методы
 const myCity = {
   city: 'Chelabinsk',
-  popular: true,
-  country: 'RU'
+  cityGreeting: function (){//можно без указания типа function
+    globalThis.console.log('Greating!')
+  }
 }
 
-//доступ к значениям объекта
-
-console.log(myCity.city);
-console.log(myCity.country);
-console.log(myCity);
-delete myCity.popular;
-console.log(myCity);
-myCity['popularNew'] = true;
-const countryProtectPypol = 'protectPipol';
-myCity[countryProtectPypol] = true;
-console.log(myCity);
-
-//глобальные объекты
-//Window - в браузере
-//global - в Node.js
- console.log(window.innerWidth)
- console.log(window.innerHeight)
-
- //globalThis - универсальный
- console.log(globalThis.innerWidth)
- globalThis.console.log(10)
+myCity.cityGreeting()

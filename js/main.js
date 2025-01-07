4f04928672464cbeda91848c0b2e7d811885a9c9
Bolter <BolterHell@gmail.com>
2025-01-07 20:56:41 +0500

    JSON lesson

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-//методы
-const myCity = {
-  city: 'Chelabinsk',
-  cityGreeting: function (){//можно без указания типа function
-    globalThis.console.log('Greating!')
-  }
+//json
+
+//JSON.parse()//Из строки в объект
+//JSON.stringify()//из объукта в строку
+
+const post = {
+  title: 'My post',
+  likesQty: 5
 }
 
-myCity.cityGreeting()
+globalThis.console.log(post)
+const postString = JSON.stringify(post)
+globalThis.console.log(postString)
+const postStringToJSON = JSON.parse(postString)
+globalThis.console.log(postStringToJSON)

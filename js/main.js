1a7cfc80cc4747056b6b76fc7590c7973ce2623d
Bolter <BolterHell@gmail.com>
2025-01-07 14:54:24 +0500

    Object

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
-function a()
-{
-  console.log("This is function\n");
-}
-a();
-//a = 10;
-//a();
+//Объекты
 
-//использовать const везде где возможно
-
-//стрелочная функция
-const b = () => {
-  console.log('Стрелочная функция');
+const myCity = {
+  city: 'Chelabinsk',
+  popular: true,
+  country: 'RU'
 }
 
-b();
-//b = 10; //выдаст ошибку
+//доступ к значениям объекта
 
+console.log(myCity.city);
+console.log(myCity.country);
+console.log(myCity);
+delete myCity.popular;
+console.log(myCity);
+myCity['popularNew'] = true;
+const countryProtectPypol = 'protectPipol';
+myCity[countryProtectPypol] = true;
+console.log(myCity);
+
+//глобальные объекты
+//Window - в браузере
+//global - в Node.js
+ console.log(window.innerWidth)
+ console.log(window.innerHeight)
+
+ //globalThis - универсальный
+ console.log(globalThis.innerWidth)
+ globalThis.console.log(10)

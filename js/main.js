104415f6cc1f5d13d67efdeead4eaf67ab284868
Bolter <BolterHell@gmail.com>
2025-01-07 21:21:36 +0500

    protection m

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
-//json
-
-//JSON.parse()//Из строки в объект
-//JSON.stringify()//из объукта в строку
-
-const post = {
-  title: 'My post',
-  likesQty: 5
+//copy Object
+//var1
+const person = {
+  name:'Bob',
+  age: 25,
+  vlog: {
+    vlogToYou: true,
+    vlogToDiscord: false
+  }
 }
 
-globalThis.console.log(post)
-const postString = JSON.stringify(post)
-globalThis.console.log(postString)
-const postStringToJSON = JSON.parse(postString)
-globalThis.console.log(postStringToJSON)
+const person2 = Object.assign({}, person)//не работает со вложенными объектами
+person2.age = 26
+
+globalThis.console.log(person2.age)
+globalThis.console.log(person.age)
+
+//var2
+//разделение объекта на свойства
+//не работает со вложенными объектами
+const person3 = { ...person}
+person3.name = 'Alice'
+person3.vlog.vlogToDiscord = true
+globalThis.console.log(person3.vlog.vlogToDiscord)
+globalThis.console.log(person.vlog.vlogToDiscord)
+
+//var3
+//Через JSON
+//Работает со вложениями объектов
+
+const person4 = JSON.parse(JSON.stringify(person))
+person4.name = 'Sergay'
+person4.vlog.vlogToYou = false
+globalThis.console.log(person4.vlog.vlogToYou)
+globalThis.console.log(person.vlog.vlogToYou)

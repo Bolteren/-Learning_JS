cd6166108c882ec220304b95535faa5794225d16
Bolter <BolterHell@gmail.com>
2025-01-07 14:22:50 +0500

    Learn object

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert('Hello World');
+const objectA = {
+  a: 10,
+  b: true
+};
+
+const coppyOfA = objectA;
+coppyOfA.c = 'abc';
+objectA.a = 20;
+console.log('text = ' + objectA.c + ' - a = ' + objectA.a);
